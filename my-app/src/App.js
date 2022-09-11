01998b9ecacc372d725ad5df6ae1f54e0d990a0a
Chiranjit Behera <chiranjitbehera34@gmail.com>
2022-09-11 23:01:05 +0530

    init

---
+++ my-app/src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [state, setState] = useState({
+    advice: "",
+  });
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{state.advice}</h1>
+        <button className="button" onClick={fetchAdvice}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
